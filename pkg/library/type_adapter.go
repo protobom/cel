be06f90bdf61d7fd855442c1401daab938fb5ab3
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2025-01-16 15:08:00 -0600

    Fix native type adapter
    This commit fixes the native type adapter. It was missing all the protobom
    elements.
    
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- pkg/library/type_adapter.go
+++ pkg/library/type_adapter.go
@@ -6,17 +6,38 @@ package library
 import (
 	"github.com/google/cel-go/common/types"
 	"github.com/google/cel-go/common/types/ref"
+	"github.com/protobom/protobom/pkg/sbom"
 
 	"github.com/protobom/cel/pkg/elements"
 )
 
+// The Protobom TypeAdapter converts native protobom elements resulting from
+// the graph API operations or evaluations into their CEL-friendly wrappers
+// that implenent ref.Val
 type TypeAdapter struct{}
 
+// NativeToValue converts from the native protobom elements to their elements.*
+// wrappers so that they can be handled in the CEL environment.
 func (TypeAdapter) NativeToValue(value any) ref.Val {
-	val, ok := value.(elements.Protobom)
-	if ok {
-		return &val
+	switch v := value.(type) {
+	case elements.Protobom:
+		return &v
+	// Actual types:
+	case sbom.Document:
+		return &elements.Document{Document: &v}
+	case sbom.NodeList:
+		return &elements.NodeList{NodeList: &v}
+	case sbom.Node:
+		return &elements.Node{Node: &v}
+	// Pointers:
+	case *sbom.Document:
+		return &elements.Document{Document: v}
+	case *sbom.NodeList:
+		return &elements.NodeList{NodeList: v}
+	case *sbom.Node:
+		return &elements.Node{Node: v}
 	}
+
 	// let the default adapter handle other cases
 	return types.DefaultTypeAdapter.NativeToValue(value)
 }
